8061c533fa5beec5a2af60f210de56ef4510231b
yutooo323 <yuto.morine03@gmail.com>
2021-06-09 12:22:08 +0000

    [Add]新規投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book =Book.new(book_params)
+    book.save
+    redirect_to book_path(:id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+    def book_params
+    params.require(:book).permit( :title, :body)
   end
 end
