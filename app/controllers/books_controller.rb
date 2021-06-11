4a1094670ddd2a3ab900728421b238a0238eea50
yutooo323 <yuto.morine03@gmail.com>
2021-06-11 10:23:10 +0000

    [Add]編集機能、削除機能、詳細ページ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book =Book.new(book_params)
     book.save
-    redirect_to book_path(:id)
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -18,6 +18,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
