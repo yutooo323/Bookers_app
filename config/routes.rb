c4af16a7c17c206a4b32005d88e821e40e383b4b
yutooo323 <yuto.morine03@gmail.com>
2021-06-12 09:19:31 +0000

    [Finish]RSpecテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :books
-  get '' => 'homes#top'
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
